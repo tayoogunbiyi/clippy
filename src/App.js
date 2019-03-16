e57da82e0810d7a63e52625f621fa99368846661
Eyitayo <eyitayoogunbiyi@gmail.com>
2019-03-16 13:44:44 +0100

    Fully set up electron with cra

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
+// this import strategy is due to the fact that CRA uses webpack & this conflicts with the standard module
+
+const electron = window.require("electron");
+const { clipboard } = electron;
+
 class App extends Component {
+  componentWillMount() {}
+  readClipboard = () => {
+    return clipboard.readText();
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+        <h2>Clippy </h2>
       </div>
     );
   }
--- src/electron.main.js
+++ src/electron.main.js
@@ -15,7 +15,6 @@ function createWindow() {
   // Create the browser window.
   mainWindow = new BrowserWindow({ width: 800, height: 600 });
 
-  // and load the index.html of the app.
   mainWindow.loadURL("http://localhost:3000");
 
   // Open the DevTools.
