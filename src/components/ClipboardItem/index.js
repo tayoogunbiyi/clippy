3d73b60d27c4051137b195c38691a8db1d005fb9
Eyitayo <eyitayoogunbiyi@gmail.com>
2019-04-05 11:58:06 +0100

    Implemented control content to add ellipsis to long texts

--- src/components/ClipboardItem/index.js
+++ src/components/ClipboardItem/index.js
@@ -1,9 +1,18 @@
 import React from "react";
 import moment from "moment";
+import { Button } from "reactstrap";
+import "./index.css";
+
+const controlContent = content => {
+  return content.length > 90 ? `${content.substr(1, 88)}  ...` : content;
+};
 
 export default ({ content, copiedAt, id, handleDelete }) => (
-  <h3>
-    {content} @ {moment(copiedAt).format("MMMM Do YYYY, h:mm:ss a")}
-    <button onClick={() => handleDelete(id)}>Delete</button>
-  </h3>
+  <div className="card">
+    <p>{controlContent(content)} </p>
+    <h6>{moment(copiedAt).format("MMMM Do YYYY, h:mm:ss a")}</h6>
+    <Button color="primary" onClick={() => handleDelete(id)}>
+      Delete
+    </Button>
+  </div>
 );
